4bc3f208eff5c5f8b204fc658ce6d005351f2971
José Santos <sjse7780@gmail.com>
2022-12-04 19:16:18 +0000

    Long Methods,Lazy Class,Dead Code with Substitute algortihm

--- src/main/java/pt/up/fe/ldts/example1/Order.java
+++ src/main/java/pt/up/fe/ldts/example1/Order.java
@@ -5,33 +5,25 @@ import java.util.List;
 
 public class Order {
     private List<OrderLine> lines;
-
     public Order() {
         this.lines = new ArrayList<>();
     }
-
     public void add(Product product, int quantity) {
         lines.add(new OrderLine(product, quantity));
     }
-
     private double getTotal() {
         double total = 0;
         for (OrderLine line : lines)
-            total += line.getProduct().getPrice() * line.getQuantity();
+            total += line.getTotal();
         return total;
     }
-
     public boolean isElegibleForFreeDelivery() {
         return getTotal() > 100;
     }
-
     public String printOrder() {
         StringBuffer printBuffer = new StringBuffer();
         for (OrderLine line : lines)
-            printBuffer.append(line.getProduct().getName() +
-                    "(x" + line.getQuantity() + "): " +
-                    (line.getProduct().getPrice() * line.getQuantity()) +
-                    "\n");
+            printBuffer.append(line.getLineString());
         printBuffer.append("Total: " + getTotal());
         return printBuffer.toString();
     }
--- src/main/java/pt/up/fe/ldts/example1/OrderLine.java
+++ src/main/java/pt/up/fe/ldts/example1/OrderLine.java
@@ -3,17 +3,14 @@ package pt.up.fe.ldts.example1;
 public class OrderLine {
     private Product product;
     private int quantity;
-
     public OrderLine(Product product, int quantity) {
         this.product = product;
         this.quantity = quantity;
     }
-
-    public Product getProduct() {
-        return product;
+    public double getTotal(){
+        return product.getPrice() * quantity;
     }
-
-    public int getQuantity() {
-        return quantity;
+    public String getLineString() {
+        return product.getName() + "(x" + quantity + "): " + getTotal() + "\n";
     }
 }
