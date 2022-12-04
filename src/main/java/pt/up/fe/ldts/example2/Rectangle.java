db8c7eec6c3be55cddaa9ac986e85bd366db0cc0
José Santos <sjse7780@gmail.com>
2022-12-04 19:56:34 +0000

    Second example- Used Replace Method with Method Object, making Shape an Abstract Class and Circle/Rectangle implementit

---
+++ src/main/java/pt/up/fe/ldts/example2/Rectangle.java
@@ -0,0 +1,26 @@
+package pt.up.fe.ldts.example2;
+
+public class Rectangle extends Shape{
+    private double width;
+    private double height;
+    public Rectangle(double x,double y,double width,double height){
+        super(x,y);
+        this.height=height;
+        this.width=width;
+    }
+    @Override
+    public double getArea() {
+        return width*height;
+    }
+    @Override
+    public double getPerimeter() {
+        return 2 *width +2*height;
+    }
+    @Override
+    public void draw(GraphicFramework graphics) {
+        graphics.drawLine(getX(), getY(), getX()+width,getY());
+        graphics.drawLine(getX()+width, getY(), getX()+width,getY()+height);
+        graphics.drawLine(getX()+width, getY()+height, getX(),getY()+height);
+        graphics.drawLine(getX(), getY()+height, getX(),getY());
+    }
+}
